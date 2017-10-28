52b9db2ceda55df2ed10389eceec45e3d46af052
fanich37 <icima.fan@gmail.com7>
2017-10-28 14:21:30 +0300

    default classes fix

--- dist/assets/scripts/select-project.min.js
+++ dist/assets/scripts/select-project.min.js
@@ -18,8 +18,18 @@
 		}(Element.prototype));
 	}
 
-
 	window.Select = Select || {};
+
+	var defaultClasses = {
+		newSelectClass: 'select',
+		buttonClass: 'select__title',
+		textClass: 'select__text',
+		iconClass: 'select__icon',
+		listWrapperClass: 'select__wrapper',
+		listClass: 'select__list',
+		listItemClass: 'select__item',
+		listItemSelectedClass: 'select__item--selected'
+	};
 
 	function Select(obj) {
 		this.selectEl = obj.selector;
@@ -29,17 +39,6 @@
 		this.listWrapper = null;
 		this.select = document.querySelector(this.selectEl);
 		
-		this.defaults = {
-			newSelectClass: 'select',
-			buttonClass: 'select__title',
-			textClass: 'select__text',
-			iconClass: 'select__icon',
-			listWrapperClass: 'select__wrapper',
-			listClass: 'select__list',
-			listItemClass: 'select__item',
-			listItemSelectedClass: 'select__item--selected'
-		};
-
 		createNewSelect.apply(this, [this.selectEl, getSelectAttributes(this.selectEl), getOptionAttributes(this.selectEl)]);
 		initClickEvent.call(this);
 	}
@@ -68,44 +67,44 @@
 		hideSelectEl(el);
 
 		this.newSelect = document.createElement('DIV');
-		this.newSelect.className = this.defaults.newSelectClass
+		this.newSelect.className = defaultClasses.newSelectClass
 		selectArray.forEach(function(item) {
 			this.newSelect.setAttribute('data-' + item.name, item.value);
 		}, this);
 
 		this.button = document.createElement('A');
-		this.button.className = this.defaults.buttonClass;
+		this.button.className = defaultClasses.buttonClass;
 		this.button.href = '/';
 
 		this.text = document.createElement('SPAN');
-		this.text.className = this.defaults.textClass;
+		this.text.className = defaultClasses.textClass;
 
 		this.icon = document.createElement('SPAN');
-		this.icon.className = this.defaults.iconClass;
+		this.icon.className = defaultClasses.iconClass;
 
 		this.listWrapper = document.createElement('DIV');
-		this.listWrapper.className = this.defaults.listWrapperClass;
+		this.listWrapper.className = defaultClasses.listWrapperClass;
 
 		this.list = document.createElement('UL');
-		this.list.className = this.defaults.listClass;
+		this.list.className = defaultClasses.listClass;
 
 		optionsArray.forEach(function(item, i) {
 			var _this = this;
 
 			this.listItem = document.createElement('LI');
-			this.listItem.className = this.defaults.listItemClass;
+			this.listItem.className = defaultClasses.listItemClass;
 			this.listItem.textContent = getOptionInnerText(el)[i];
 			if (item.length) {
 				item.forEach(function(attr) {
 					if (attr.value === 'selected') {
-						this.listItem.classList.add(this.defaults.listItemSelectedClass);
+						this.listItem.classList.add(defaultClasses.listItemSelectedClass);
 						this.text.textContent = getOptionInnerText(el)[i];
 					}
 					this.listItem.setAttribute('data-' + attr.name, attr.value);
 				}, _this);
 			}else {
 				if (!i) {
-					this.listItem.classList.add(this.defaults.listItemSelectedClass);
+					this.listItem.classList.add(defaultClasses.listItemSelectedClass);
 					this.listItem.setAttribute('data-selected', 'selected');
 					this.text.textContent = getOptionInnerText(el)[i];
 				}
@@ -163,11 +162,11 @@
 
 	function changeSelectedAttrAndClass(e) {
 		var newSelectedItem = e.target.closest('.select__item');
-		var selectedItem = newSelectedItem.parentElement.querySelector('.' + this.defaults.listItemSelectedClass);
+		var selectedItem = newSelectedItem.parentElement.querySelector('.' + defaultClasses.listItemSelectedClass);
 		
-		selectedItem.classList.remove(this.defaults.listItemSelectedClass);
+		selectedItem.classList.remove(defaultClasses.listItemSelectedClass);
 		selectedItem.removeAttribute('data-selected');
-		newSelectedItem.classList.add(this.defaults.listItemSelectedClass);
+		newSelectedItem.classList.add(defaultClasses.listItemSelectedClass);
 		newSelectedItem.setAttribute('data-selected', 'selected');
 	}
 
